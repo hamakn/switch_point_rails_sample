fa7582656624ac8755f473ab66248dcdba583e06
Katsunori Kawaguchi <hamakata@gmail.com>
2016-04-15 20:54:37 +0900

    Activate switch_point auto_writable only in rails console

--- config/initializers/switch_point.rb
+++ config/initializers/switch_point.rb
@@ -2,4 +2,9 @@ SwitchPoint.configure do |config|
   config.define_switch_point :default,
     readonly: :"#{Rails.env}_slave",
     writable: :"#{Rails.env}_master"
+
+  # activate switch_point auto_writable only in rails console
+  Rails::Application.console do
+    config.auto_writable = true
+  end
 end
